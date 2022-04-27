480eaeb6906516b555b5d97eefda90bd91f5bcb2
garysantana3 <104527536+garysantana3@users.noreply.github.com>
2022-04-27 14:37:03 -0500

    Add files via upload

---
+++ app/src/main/java/com/gosp/apps/mlapp/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.gosp.apps.mlapp.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.gosp.apps.mlapp.android.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
