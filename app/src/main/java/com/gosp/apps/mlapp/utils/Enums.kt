1f8611e1ddd0db3adb9b1fe54ed931af726fae7a
Gary Santana <gary.santana.dev@gmail.com>
2022-05-09 13:43:49 -0500

    UPDATE: ENUMS KT

---
+++ app/src/main/java/com/gosp/apps/mlapp/utils/Enums.kt
@@ -0,0 +1,12 @@
+package com.gosp.apps.mlapp.mlapp.utils
+
+class Enums {
+
+    enum class ViewStates {
+        SEARCH,
+        SEARCH_RESULTS,
+        PRODUCT_DETAIL,
+        NOT_INTERNET,
+        SOMETHING_WRONG
+    }
+}
